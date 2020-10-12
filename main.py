386f5d7dd934d30179d96f6a252cd60bc13d9182
alexgiving <alexgiving@mail.ru>
2020-10-13 00:33:34 +0300

    tested

--- main.py
+++ main.py
@@ -1,54 +1,49 @@
-
 def brute_force(i: int, ww: int, wc):
     if i < 0:
         return 0
     t = 0
-    if wc[i][0] + ww <= m:
+    if ww + wc[i][0] <= m:
         t = brute_force(i - 1, wc[i][0] + ww, wc) + wc[i][1]
-    return max(t, brute_force(i - 1, wc[i][0], wc))
+    return max(t, brute_force(i - 1, ww, wc))
 
 
 def greedy_algorithm(ww: int, mk: int, wc):
-    ans = []
-    wc = sorted(wc, key=lambda x: x[0] / x[1])
     for j in range(n):
         if wc[j][0] <= mk:
             ww += wc[j][1]  # price
             mk -= wc[j][0]  # weight
-            ans.append(wc[j][0])
+            print(wc[j])
         else:
-            ww += mk * wc[j][1]
-            ans.append(wc[j][0])
+            ww += mk * wc[j][1] / wc[j][0]
+            print(mk, "weight from", wc[j])
+            return ww
     return ww
 
 
 def merge(list1, list2):
-    return [(list1[i], list2[i]) for i in range(0, len(list1))]
+    return [(list1[i], list2[i]) for i in range(len(list1))]
 
 
 def discrete_algorithm1(mk: int, wc):
-    f = [[0] * mk for k in range(n)]
-    for j in range(n):
-        for k in range(mk):
-            if mk > wc[j][0]:
-                f[j][k] = max(f[j - 1][k], f[j - 1][k - wc[j][0]] + c[j])
-            else:
-                f[j][k] = f[j - 1][k]
-    return f[n-1][mk-1]
+    a = [[0] * mk for k in range(n)]
+    for i in range(n):
+        for j in range(mk):
+            if i > 0:
+                if j - wc[i][0] >= 0:
+                    a[i][j] = max(wc[i][1] + a[i - 1][j - wc[i][0]], a[i - 1][j])
+                else:
+                    a[i][j] = a[i - 1][j]
+            elif j >= wc[i][0]:
+                a[i][j] = wc[i][1]
+    return a[n - 1][m - 1]
 
 
 with open('backpack.txt', "r") as f:
-    m, n = next(f).split()
+    n, m = next(f).split()
     m = int(m)
     n = int(n)
-    k = 0
-    for line in f:
-        if 0 == k:
-            w = list(map(int, list(line.split())))
-            k += 1
-        if 1 == k:
-            c = list(map(int, list(line.split())))
-    wc = merge(w, c)
-    print(brute_force(n - 1, 0, wc))
-    print(greedy_algorithm(0, m, wc))
-    print(discrete_algorithm1(m, wc))
+    wc = sorted(merge(list(map(int, next(f).split())),
+                      list(map(int, next(f).split()))), key=lambda x: x[0] / x[1])
+    print("\nTask 1\n", brute_force(n - 1, 0, wc))
+    print("\nTask 2"), print("Total coast: ", greedy_algorithm(0, m, wc))
+    print("\nTask 3\nTotal coast: ", discrete_algorithm1(m, wc))
